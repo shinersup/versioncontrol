13fdd658b72df8abe8df9cf9c48f7f5dcbdd9002
Shiner <sjs.shiner@gmail.com>
2023-10-25 13:44:15 -0400

    Includes encode and main functions

--- version_control.py
+++ version_control.py
@@ -8,23 +8,27 @@ def main():
             encoded_digit = str(int(digit) + 3)
             encoded_password += encoded_digit
         return encoded_password
+    
+    # Add decoder function here
 
     def menu():
         print('''Menu
-    1. Encode
-    2. Decode
-    3. Exit''')
+-------------
+1. Encode
+2. Decode
+3. Quit''')
 
     while True:
         menu()
-        option = int(input('Choose an option: '))
+        option = int(input('Please enter an option: '))
         if option == 1:
-            user_input = input('Please enter a password to encode: ')
-            result = encoder(user_input)
-            print(result)
+            user_input = input('Please enter your password to encode: ')
+            encoded_result = encoder(user_input)
+            print('Your password has been encoded and stored!')
         if option == 2:
-            user_input = int(input('Please enter a password to encode: '))
-            result = decoder(user_input)
+            user_input = encoded_result
+            decoded_result = decoder(user_input)
+            print(f'The encoded password is {encoded_result}, and the original password is {decoded_result}')
         if option == 3:
             exit()
 
