5424041ed41b6c56aa1001ccd9c637dd20342a95
Shiner <sjs.shiner@gmail.com>
2023-10-25 14:03:43 -0400

    Includes encode and main functions

--- version_control.py
+++ version_control.py
@@ -1,15 +1,13 @@
+# Code by Shiner Supre
 def main():
 
-    def encoder(password):
-        # Encoded by Shiner
+    def encode(password):
         encoded_password = ""
         for digit in password:
             # Converts the character to an integer, adds 3 from each, and appends to an empty string
             encoded_digit = str(int(digit) + 3)
             encoded_password += encoded_digit
         return encoded_password
-    
-    # Add decoder function here
 
     def menu():
         print('''Menu
@@ -23,12 +21,8 @@ def main():
         option = int(input('Please enter an option: '))
         if option == 1:
             user_input = input('Please enter your password to encode: ')
-            encoded_result = encoder(user_input)
+            encoded_result = encode(user_input)
             print('Your password has been encoded and stored!')
-        if option == 2:
-            user_input = encoded_result
-            decoded_result = decoder(user_input)
-            print(f'The encoded password is {encoded_result}, and the original password is {decoded_result}.')
         if option == 3:
             exit()
 
