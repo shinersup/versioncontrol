5ff762215e4316b7f4a495e85fa3d471b4076c85
Shiner <sjs.shiner@gmail.com>
2023-10-25 13:21:44 -0400

    Includes encode and menu functions

--- version_control.py
+++ version_control.py
@@ -1,28 +1,32 @@
-def encoder(password):
-    # Encoded by Shiner
-    encoded_password = ""
-    for digit in password:
-        # Convert the character to an integer, add 3, and append to the list
-        encoded_digit = str(int(digit) + 3)
-        encoded_password += encoded_digit
-    return encoded_password
+def main():
 
+    def encoder(password):
+        # Encoded by Shiner
+        encoded_password = ""
+        for digit in password:
+            # Convert the character to an integer, add 3, and append to the list
+            encoded_digit = str(int(digit) + 3)
+            encoded_password += encoded_digit
+        return encoded_password
 
-def menu():
-    print('''Menu
-1. Encode
-2. Decode
-3. Exit''')
+    def menu():
+        print('''Menu
+    1. Encode
+    2. Decode
+    3. Exit''')
 
-while True:
-    menu()
-    option = int(input('Choose an option: '))
-    if option == 1:
-        user_input = input('Please enter a password to encode: ')
-        result = encoder(user_input)
-        print(result)
-    if option == 2:
-        user_input = int(input('Please enter a password to encode: '))
-        result = decoder(user_input)
-    if option == 3:
-        exit()
+    while True:
+        menu()
+        option = int(input('Choose an option: '))
+        if option == 1:
+            user_input = input('Please enter a password to encode: ')
+            result = encoder(user_input)
+            print(result)
+        if option == 2:
+            user_input = int(input('Please enter a password to encode: '))
+            result = decoder(user_input)
+        if option == 3:
+            exit()
+
+if __name__ == '__main__':
+    main()
