de7f173312640122614f1a5e2030c2839a583831
Nick Espinoza <espinoza.nick88@gmail.com>
2023-10-31 02:24:24 -0400

    added decodind & decoding option

--- version_control.py
+++ version_control.py
@@ -1,6 +1,5 @@
 # Code by Shiner Supre
 def main():
-
     def encode(password):
         encoded_password = ""
         for digit in password:
@@ -8,6 +7,12 @@ def main():
             encoded_digit = str((int(digit) + 3) % 10)
             encoded_password += encoded_digit
         return encoded_password
+
+    def decode(password):
+        decoded_pass = []
+        for i in list(password):
+            decoded_pass.append(str((int(i)-3) % 10)) # Reverses encoding process
+        return ''.join(decoded_pass)
 
     def menu():
         print('''Menu
@@ -19,12 +24,21 @@ def main():
     while True:
         menu()
         option = input('Please enter an option: ')
+        if option not in ['1', '2', '3']:
+            continue
         if option == '1':
             user_input = input('Please enter your password to encode: ')
             encoded_result = encode(user_input)
             print('Your password has been encoded and stored!')
+        if option == '2':
+            try: # checks if there is a password to decode
+                decoded_result = decode(encoded_result)
+                print(f'The encoded password is {encoded_result}, and the original password is {decoded_result}')
+            except: 
+                print('Please encode a password first.')
         if option == '3':
             exit()
+
 
 if __name__ == '__main__':
     main()
